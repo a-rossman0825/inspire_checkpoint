3328eae4317d4b0797ac6dd276348e78a271a10d
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-06-13 15:27:59 -0600

    Get Todos

--- app/controllers/TodosController.js
+++ app/controllers/TodosController.js
@@ -1,9 +1,21 @@
+import { AppState } from "../AppState.js";
+import { todosService } from "../services/TodosService.js";
+import { Pop } from "../utils/Pop.js";
 
 
 export class TodosController {
   constructor() {
-
+    console.log("🗒️🎛️ Ready!");
+    AppState.on("identity", this.getTodos);
   }
 
-  
+  async getTodos() {
+    try {
+      await todosService.getTodos();
+    } catch (error) {
+      Pop.error(error, "Failure:", "Could not get Todos");
+      console.error("🗒️🎛️ getTodos failed", error);
+    }
+  }
+
 }
--- app/services/TodosService.js
+++ app/services/TodosService.js
@@ -1,9 +1,15 @@
+import { api } from "../utils/Axios.js";
 
 
 class TodosService {
 
+  async getTodos() {
+    const res = await api.get('api/todos');
+    console.log('🗒️🦮got Todos!', res.data);
 
-  
+  }
+
+
 }
 
 export const todosService = new TodosService();
